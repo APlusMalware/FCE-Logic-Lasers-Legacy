4ae5e1dadc5b25f7d5f27433d304e79fe4c65ca7
A+ Malware <zyzzyva88@hotmail.com>
2015-11-29 22:30:14 -0600

    Add disassembly of originals

---
+++ Originals/Disassemble.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\ildasm" /OUTPUT="temp.il" "Latest.dll"
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm.exe" /DLL "temp.il"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\ildasm" /OUTPUT="Latest.il" "temp.dll"
+del temp.dll temp.il temp.res

6129f6279b9b0ee3f4a4d0a9de62d82d0c0be350
A+ Malware <zyzzyva88@hotmail.com>
2015-12-12 12:13:50 -0600

    Add capability to replace a portion of the file and use this with EntityManager.

--- Builds/Build.cs
+++ Builds/Build.cs
@@ -8,6 +8,7 @@ class CILSwapper
 		String full = File.ReadAllText(@"..\Originals\Latest.il");
 		String[] originalFragmentPaths = Directory.GetFiles(@"..\Originals\Fragments");
 		String[] modifiedFragmentPaths = Directory.GetFiles(@"..\Fragments");
+		String[] originalReplacePaths = Directory.GetFiles(@"..\Originals\Fragments\Replace");
 		Int32 insertIndex = -1;
 
 		foreach (String originalPath in originalFragmentPaths)
@@ -22,7 +23,7 @@ class CILSwapper
 			}
 			
 			full = full.Substring(0, start) + full.Substring(start + originalFragment.Length);
-			if (insertIndex == -1)
+			if (insertIndex == -1 || start < insertIndex)
 				insertIndex = start;
 		}
 
@@ -32,6 +33,23 @@ class CILSwapper
 			full = full.Insert(insertIndex, modifiedFragment);
 		}
 
+		Console.WriteLine("Beginning replacements...");
+		foreach (String originalPath in originalReplacePaths)
+		{
+			String originalFragment = File.ReadAllText(originalPath);
+			Int32 start = full.IndexOf(originalFragment, StringComparison.Ordinal);
+			if (start == -1)
+			{
+				// ERROR 
+                Console.WriteLine("Contents of file " + originalPath + " not found!");
+				return;
+			}
+			String modifiedFragment = File.ReadAllText(Path.Combine(@"..\Fragments\Replace", Path.GetFileName(originalPath)));
+			
+			full = full.Substring(0, start) + full.Substring(start + originalFragment.Length);
+			full = full.Insert(start, modifiedFragment);
+		}
+		
 		Console.WriteLine("Replacement completed. Beginning file write...");
 		File.WriteAllText(@"..\Integrated.il", full);
 		Console.WriteLine("File write complete.");

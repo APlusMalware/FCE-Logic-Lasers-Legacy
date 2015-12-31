062c3cf38bcde8c5fd7091a9a690ec729805e65b
A+ Malware <zyzzyva88@hotmail.com>
2015-12-31 11:26:21 -0600

    Make disassembly quieter

--- Originals/Disassemble.bat
+++ Originals/Disassemble.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\ildasm" /NOBAR /OUTPUT="temp.il" "Assembly-CSharp.dll"
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm.exe" /DLL "temp.il"
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm.exe" /DLL "temp.il" /quiet
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\ildasm" /NOBAR /OUTPUT="Latest.il" "temp.dll"
 del temp.dll temp.il temp.res Assembly-CSharp.dll
